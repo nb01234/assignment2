abb26546c9542b15beddab56f00f2e9b72e11f89
342628146 <342628146@C-5W0XS13.tdsb.on.ca>
2025-05-14 10:22:14 -0400

    h

---
+++ src/Student.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author 342628146
+ */
+public class Student extends User{
+    private double studyHours = 0;
+    
+    public Student(String name, int age, double studyHours) {
+        super(name, age);
+        this.studyHours = studyHours;
+    }
+    
+    public Student(String name, int age, double studyHours) {
+        super(name, age);
+        this.studyHours = studyHours;
+    }
+}
