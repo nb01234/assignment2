28f4ea6e7b8484c7cb118d603b36e5a100e14735
342468766 <342468766@C-9TZP0S2.tdsb.on.ca>
2025-05-14 10:09:44 -0400

    Second

---
+++ src/User.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 342468766
+ */
+public class User {
+    private String name;
+    private int age;
+    private static String DEFAULT_NAME = "Unkown";
+    private static int DEFAULT_AGE = 0;
+    private static int numPeopele = 0;
+    
+    public User(String name, int age) {
+        
+    }
+}
