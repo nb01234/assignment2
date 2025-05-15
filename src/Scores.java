15e27f04666e5b77ce0546b7b405767ad7aef9be
342468766 <342468766@C-9TZP0S2.tdsb.on.ca>
2025-05-15 10:56:21 -0400

    Second

--- src/Scores.java
+++ src/Scores.java
@@ -7,12 +7,12 @@
  *
  * @author 342468766
  */
-public class Scoreboard extends javax.swing.JFrame {
+public class Scores extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
      */
-    public Scoreboard() {
+    public Scores() {
         initComponents();
     }
 
@@ -58,21 +58,23 @@ public class Scoreboard extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Scoreboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Scores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Scoreboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Scores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Scoreboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Scores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Scoreboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Scores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Scoreboard().setVisible(true);
+                new Scores().setVisible(true);
             }
         });
     }
